4c146d2471b93d7eee29fd253b675fffdba22efe
Paul Vencill <pvencill@mitre.org>
2014-11-14 11:29:00 -0500

    added some test data

--- test/events.js
+++ test/events.js
@@ -1,25 +1,33 @@
 var Zip2Git = require('../');
+var should = require('should');
+
+// TODO: need a test archive
+// Need a test git repo
+// Need to reset the git repo to the initial commit before the successful commit tests kick off
+
+function expectError(done){
+  return function(e){
+    should.exist(e);
+    done();
+  };
+}
 
 describe('#commit', function(){
+  var z2g = new Zip2Git();
   describe('with null inputs', function(){
-    var z2g = new Zip2Git();
     it('should emit a standard error event', function(done){
-      z2g.on('error', function(e){
-        done();
-      });
-
-      z2g.commit();
+      z2g.commit(null, null, expectError(done));
     });
   });
 
   describe('with empty string inputs', function(){
-    var z2g = new Zip2Git();
     it('should emit a standard error event', function(done){
-      z2g.on('error', function(e){
-        done();
-      });
-
-      z2g.commit('', '');
+      z2g.commit('', '', expectError(done));
     });
+  });
+
+  describe('with good inputs', function(){
+    it('should put nonexistent files from the zip archive into the repository');
+    it('should modify existing files to match what is in the zip archive');
   });
 });
