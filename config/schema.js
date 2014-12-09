e08c3fc9bf29f60e448158188186f2ee5fdd2ced
Steve Purdy <sfpurdy@mitre.org>
2014-12-09 16:15:56 -0500

    Added username and password options

--- config/schema.js
+++ config/schema.js
@@ -20,8 +20,21 @@ var conf = convict({
   message:{
     doc:'The commit message',
     default:'Unzipping %s into %s',
+    env: 'ZIP_GIT_MSG'
     arg:'message'
-  }
+  },
+  username:{
+    doc:'The username to use for git tranactions',
+    default: '',
+    env: 'ZIP_GIT_USER',
+    arg: 'zip-git-user'
+  },
+  password:{
+      doc:'The password to use for git tranactions',
+      default: '',
+      env: 'ZIP_GIT_PWD',
+      arg: 'zip-git-pwd'
+  },
 });
 
 module.exports = conf;
