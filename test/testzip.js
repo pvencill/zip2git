d403430586b89dd7704fd95049f5a523815b9d62
Steve Purdy <sfpurdy@mitre.org>
2014-12-18 09:18:35 -0500

    Fixing bad commits

--- test/testzip.js
+++
@@ -1,9 +0,0 @@
-var zip = require('../');
-var zippy = new zip();
-
-
-var firefly = function(err) { if(err) console.log('err=',err); else console.log('success');}
-var remote = process.env.ZIP_TEST_REMOTE;
-
-console.log('cwd=', process.cwd());
-zippy.commit('../testzip.zip', remote,{commitMsg:'third'},firefly);
